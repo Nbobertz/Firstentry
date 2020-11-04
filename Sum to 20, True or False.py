7eb089677e396a606803ee49756f1fd4d54f57e0
Nbobertz <nbobertz@gmail.com>
2020-11-04 17:19:53 -0500

    Sum to 20, True or False.py

---
+++ Sum to 20, True or False.py
@@ -0,0 +1,6 @@
+def isthis20(n1,n2):
+    #this function is designed to see if there is a 20 or if both n1 or n2 add up to 20. If so it will print True
+    if n1+n2 == 20 or n1==20 or n2==20:
+        return True
+    else:
+        return False
